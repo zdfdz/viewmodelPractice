4d69b58be9aba1b56646daf5520f376548174fc7
zdfdz <zdfdz@outlook.com>
2021-05-16 16:16:47 +0800

    init project

---
+++ app/src/main/java/study/zdf/viewmodeldemo5_16/MainActivity.kt
@@ -0,0 +1,12 @@
+package study.zdf.viewmodeldemo5_16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
